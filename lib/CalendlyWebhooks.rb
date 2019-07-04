312ee9b8426e3727c9abcd44bb6504e7e9ebdfe2
Grayson <graysonpberger@gmail.com>
2019-07-04 12:06:12 -0400

    Added additional required files

--- lib/CalendlyWebhooks.rb
+++ lib/CalendlyWebhooks.rb
@@ -1,7 +1,8 @@
-require "CalendlyWebhooks/version"
-require "CalendlyWebhooks/CLI"
+require_relative "./CalendlyWebhooks/version"
+require_relative "./CalendlyWebhooks/CLI"
+#require "CalendlyWebhooks/CLI"
 
-# module CalendlyWebhooks
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
+module CalendlyWebhooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
