c32207ac4daeeae271024a5a31840ae412881163
Grayson <graysonpberger@gmail.com>
2019-07-07 16:52:45 -0400

    add methods to CLI class

--- CalendlyWebhooks.gemspec
+++ CalendlyWebhooks.gemspec
@@ -40,7 +40,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "pry"
   spec.add_dependency "nokogiri"
-  spec.add_dependency "open-uri"
+  spec.add_dependency "httparty"
   #httpparty
 
 end
--- lib/CalendlyWebhooks/CLI.rb
+++ lib/CalendlyWebhooks/CLI.rb
@@ -4,7 +4,63 @@ class CalendlyWebhooks::CLI
     def call
         puts "Welcome to Calendly's Developer Help Center! What would you like to do today?"
         choice
+        input = ""
+        input = gets.strip
+        while input != "exit"
+        case input
+        when input == 1
+            create_webhook
+            optional_exit
+            input = gets.strip
+        when input == 2
+            puts "Do you know the ID of the webhook that you would like to delete? (y/n)"
+            id = gets.strip
+            if id == "y"
+                delete_webhook
+                optional_exit
+                input = gets.strip
+            elsif id == "n"
+                puts "You will be unable to delete a webhook without the ID, please try finding the webhook first using option 3 from the main menu"
+                input = "return"
+            end
+        when input == 3
+            find_webhooks
+            optional_exit
+            input = gets.strip
+        when input == 4
+            sample_webhook_data
+            optional_exit
+            input = gets.strip
+        when input == 5
+            see_event_types
+            optional_exit
+            input = gets.strip
+        when input == 6
+            see_me
+            optional_exit
+            input = gets.strip
+        when input == return
+            choice
+        else
+            puts "Please enter a valid number 1-6 or 'exit' to end the program"
+        end
+        if input == "exit"
+            puts "For security reasons, this program will self destruct in 5..."
+            sleep(1) 
+            puts "4..."
+            sleep(1) 
+            puts "3..." 
+            sleep(1) 
+            puts "2..." 
+            sleep(1) 
+            puts "1..." 
+            sleep(1)
+            puts "BOOM!"
+            
+        end 
+
     end
+end
 
     def choice
         puts "1. Create a webhook"
@@ -13,6 +69,202 @@ class CalendlyWebhooks::CLI
         puts "4. See sample webhook data"
         puts "5. See my event types"
         puts "6. See information about me"
+        puts "Type exit at any time to end the program"
     end
 
+    def optional_exit
+        puts "Would you like to exit or return to main menu?"
+        puts "Type 'exit' to exit the program or 'return' to return to the main menu"
+    end
+
+    def create_webhook
+        hook = CalendlyWebhooks::Request.create_from_scraper("https://developer.calendly.com/docs/webhook-subscriptions")
+        doc = hook.documentation
+        kind = hook.type
+        puts "What is your API token?"
+        key = gets.strip
+        hook.token = key
+        puts "What is your webhook destination URL"
+        destination = gets.strip
+        hook.webhook_url = destination
+        hook.endpoint_url = "https://calendly.com/api/v1/hooks"
+        hook.make_request
+        puts "We are are using the #{doc} documentation with a #{kind} request to accomplish this for you"
+        sleep(1)
+        puts "Webhook created!"
+        puts "Feel free to test out your new webhook by booking a test event with yourself using your Calendly scheduling link!"
+    end
+    
+    def delete_webhook
+        puts "What is the Hook ID of the webhook that you would like to delete?"
+        id = gets.strip
+        hook = CalendlyWebhooks::Request.create_from_scraper("https://developer.calendly.com/docs/delete-webhook-subscription")
+        hook.endpoint_url = "https://calendly.com/api/v1/#{id}"
+        doc = hook.documentation
+        kind = hook.type
+        puts "What is your API token?"
+        key = gets.strip
+        hook.token = key
+        puts "We are are using the #{doc} documentation with a #{kind} request to accomplish this for you"
+        hook.make_request
+        sleep(1)
+        puts "Webhook deleted!"
+    end
+
+    def find_webhooks
+        hook = CalendlyWebhooks::Request.create_from_scraper("https://developer.calendly.com/docs/get-list-of-webhook-subscriptions")
+        hook.endpoint_url = "https://calendly.com/api/v1/hooks"
+        doc = hook.documentation
+        kind = hook.type
+        puts "What is your API token?"
+        key = gets.strip
+        hook.token = key
+        puts "We are are using the #{doc} documentation with a #{kind} request to accomplish this for you"
+        hook.make_request
+        sleep(1)
+        puts "Here is the webhook data associated with that API token"
+        puts hook.res.body
+    end
+
+    def sample_webhook_data
+        puts "  \n
+  'event':'invitee.created', \n
+  'time':'2018-03-14T19:16:01Z', \n
+  'payload':{ \n
+    'event_type':{   \n
+      'uuid':'CCCCCCCCCCCCCCCC', \n
+      'kind':'One-on-One', \n
+      'slug':'event_type_name', \n
+      'name':'Event Type Name', \n
+      'duration':15, \n
+      'owner':{   \n
+        'type':'users', \n
+        'uuid':'DDDDDDDDDDDDDDDD' \n
+      } \n
+    }, \n
+    'event':{   \n
+      'uuid':'BBBBBBBBBBBBBBBB', \n
+      'assigned_to':[   \n
+        'Jane Sample Data' \n
+      ], \n
+      'extended_assigned_to':[   \n
+        {   \n
+          'name':'Jane Sample Data', \n
+          'email':'user@example.com', \n
+          'primary':false \n
+        } \n
+      ], \n
+      'start_time':'2018-03-14T12:00:00Z', \n
+      'start_time_pretty':'12:00pm - Wednesday, March 14, 2018', \n
+      'invitee_start_time':'2018-03-14T12:00:00Z', \n
+      'invitee_start_time_pretty':'12:00pm - Wednesday, March 14, 2018', \n
+      'end_time':'2018-03-14T12:15:00Z', \n
+      'end_time_pretty':'12:15pm - Wednesday, March 14, 2018', \n
+      'invitee_end_time':'2018-03-14T12:15:00Z', \n
+      'invitee_end_time_pretty':'12:15pm - Wednesday, March 14, 2018', \n
+      'created_at':'2018-03-14T00:00:00Z', \n
+      'location':'The Coffee Shop', \n
+      'canceled':false, \n
+      'canceler_name':null, \n
+      'cancel_reason':null, \n
+      'canceled_at':null \n
+    }, \n
+    'invitee':{   \n
+      'uuid':'AAAAAAAAAAAAAAAA', \n
+      'first_name':'Joe', \n
+      'last_name':'Sample Data', \n
+      'name':'Joe Sample Data', \n
+      'email':'not.a.real.email@example.com',\n
+      'text_reminder_number':'+14045551234', \n
+      'timezone':'UTC', \n
+      'created_at':'2018-03-14T00:00:00Z', \n
+      'is_reschedule':false, \n
+      'payments':[   \n
+        {   \n
+          'id':'ch_AAAAAAAAAAAAAAAAAAAAAAAA', \n
+          'provider':'stripe', \n
+          'amount':1234.56, \n
+          'currency':'USD', \n
+          'terms':'sample terms of payment (up to 1,024 characters)', \n
+          'successful':true \n
+        } \n
+      ], \n
+      'canceled':false, \n
+      'canceler_name':null, \n
+      'cancel_reason':null, \n
+      'canceled_at':null \n
+    }, \n
+    'questions_and_answers':[   \n
+      {   \n
+        'question':'Skype ID', \n
+        'answer':'fake_skype_id' \n
+      }, \n
+      {   \n
+        'question':'Facebook ID', \n
+        'answer':'fake_facebook_id' \n
+      }, \n
+      {   \n
+        'question':'Twitter ID', \n
+        'answer':'fake_twitter_id' \n
+      }, \n
+      {   \n
+        'question':'Google ID', \n
+        'answer':'fake_google_id' \n
+      } \n
+    ], \n
+    'questions_and_responses':{   \n
+      '1_question':'Skype ID', \n
+      '1_response':'fake_skype_id', \n
+      '2_question':'Facebook ID', \n
+      '2_response':'fake_facebook_id', \n
+      '3_question':'Twitter ID', \n
+      '3_response':'fake_twitter_id', \n
+      '4_question':'Google ID', \n
+      '4_response':'fake_google_id' \n
+    }, \n
+    'tracking':{   \n
+      'utm_campaign':null, \n
+      'utm_source':null, \n
+      'utm_medium':null, \n
+      'utm_content':null, \n
+      'utm_term':null, \n
+      'salesforce_uuid':null \n
+    }, \n
+    'old_event':null, \n
+    'old_invitee':null, \n
+    'new_event':null, \n
+    'new_invitee':null \n
+  } \n
+}"
+    end
+    
+    def see_event_types
+        hook = CalendlyWebhooks::Request.create_from_scraper("https://developer.calendly.com/docs/user-event-types")
+        hook.endpoint_url = "https://calendly.com/api/v1/users/me/event_types"
+        doc = hook.documentation
+        kind = hook.type
+        puts "What is your API token?"
+        key = gets.strip
+        hook.token = key
+        puts "We are are using the #{doc} documentation with a #{kind} request to accomplish this for you"
+        hook.make_request
+        sleep(1)
+        puts "Here are the event types associated with that API token!"
+        puts hook.res.body
+    end
+
+    def see_me
+        hook = CalendlyWebhooks::Request.create_from_scraper("https://developer.calendly.com/docs/about-me")
+        hook.endpoint_url = "https://calendly.com/api/v1/users/me"
+        doc = hook.documentation
+        kind = hook.type
+        puts "What is your API token?"
+        key = gets.strip
+        hook.token = key
+        puts "We are are using the #{doc} documentation with a #{kind} request to accomplish this for you"
+        hook.make_request
+        sleep(1)
+        puts "Here is the information about the user associated with that API token!"
+        puts hook.res.body
+    end
 end
